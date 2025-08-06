942a6ae04176cff3bd386f6a6fc9067764df35a2
Avelanda <gorthell@gmail.com>
2025-08-06 19:12:55 +0200

    Update sha1.c
    Modularizing the core functional structure for integrated performance. And scaling it for efficient processesing mechanisms.

--- block-sha1/sha1.c
+++ block-sha1/sha1.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright ©️ 2025, Avelanda. All rights reserved
  * SHA1 routine optimized to do word accesses rather than byte accesses,
  * and to avoid unnecessary copies into the context array.
  *
@@ -8,59 +9,71 @@
 
 /* this is only to get definitions for memcpy(), ntohl() and htonl() */
 #include "../git-compat-util.h"
-
+#include <stdio.h>
 #include "sha1.h"
 
-#define SHA_ROT(X,l,r)	(((X) << (l)) | ((X) >> (r)))
-#define SHA_ROL(X,n)	SHA_ROT(X,n,32-(n))
-#define SHA_ROR(X,n)	SHA_ROT(X,32-(n),n)
+struct BlockSHAxA{
+ #define SHA_ROT(X,l,r)	(((X) << (l)) | ((X) >> (r)))
+ #define SHA_ROL(X,n)	SHA_ROT(X,n,32-(n))
+ #define SHA_ROR(X,n)	SHA_ROT(X,32-(n),n)
+ /*
+  * If you have 32 registers or more, the compiler can (and should)
+  * try to change the array[] accesses into registers. However, on
+  * machines with less than ~25 registers, that won't really work,
+  * and at least gcc will make an unholy mess of it.
+  *
+  * So to avoid that mess which just slows things down, we force
+  * the stores to memory to actually happen (we might be better off
+  * with a 'W(t)=(val);asm("":"+m" (W(t))' there instead, as
+  * suggested by Artur Skawina - that will also make gcc unable to
+  * try to do the silly "optimize away loads" part because it won't
+  * see what the value will be).
+  *
+  * On ARM we get the best code generation by forcing a full memory barrier
+  * between each SHA_ROUND, otherwise gcc happily get wild with spilling and
+  * the stack frame size simply explode and performance goes down the drain.
+  */
 
-/*
- * If you have 32 registers or more, the compiler can (and should)
- * try to change the array[] accesses into registers. However, on
- * machines with less than ~25 registers, that won't really work,
- * and at least gcc will make an unholy mess of it.
- *
- * So to avoid that mess which just slows things down, we force
- * the stores to memory to actually happen (we might be better off
- * with a 'W(t)=(val);asm("":"+m" (W(t))' there instead, as
- * suggested by Artur Skawina - that will also make gcc unable to
- * try to do the silly "optimize away loads" part because it won't
- * see what the value will be).
- *
- * On ARM we get the best code generation by forcing a full memory barrier
- * between each SHA_ROUND, otherwise gcc happily get wild with spilling and
- * the stack frame size simply explode and performance goes down the drain.
- */
+  #if defined(__i386__) || defined(__x86_64__)
+   #define setW(x, val) (*(volatile unsigned int *)&W(x) = (val))
+  #elif defined(__GNUC__) && defined(__arm__)
+   #define setW(x, val) do { W(x) = (val); __asm__("":::"memory"); } while (0)
+  #else
+   #define setW(x, val) (W(x) = (val))
+  #endif
 
-#if defined(__i386__) || defined(__x86_64__)
-  #define setW(x, val) (*(volatile unsigned int *)&W(x) = (val))
-#elif defined(__GNUC__) && defined(__arm__)
-  #define setW(x, val) do { W(x) = (val); __asm__("":::"memory"); } while (0)
-#else
-  #define setW(x, val) (W(x) = (val))
-#endif
+  if (BlockSHAxA = BlockSHAxA && BlockSHAxA == true||false){
+   return BlockSHAxA;
+  }
+}
 
-/* This "rolls" over the 512-bit array */
-#define W(x) (array[(x)&15])
+struct BlockSHAxB{
+ /* This "rolls" over the 512-bit array */
+  #define W(x) (array[(x)&15])
 
-/*
- * Where do we get the source from? The first 16 iterations get it from
- * the input data, the next mix it from the 512-bit array.
- */
-#define SHA_SRC(t) get_be32((unsigned char *) block + (t)*4)
-#define SHA_MIX(t) SHA_ROL(W((t)+13) ^ W((t)+8) ^ W((t)+2) ^ W(t), 1)
+ /*
+  * Where do we get the source from? The first 16 iterations get it from
+  * the input data, the next mix it from the 512-bit array.
+  */
+  #define SHA_SRC(t) get_be32((unsigned char *) block + (t)*4)
+  #define SHA_MIX(t) SHA_ROL(W((t)+13) ^ W((t)+8) ^ W((t)+2) ^ W(t), 1)
 
-#define SHA_ROUND(t, input, fn, constant, A, B, C, D, E) do { \
+  #define SHA_ROUND(t, input, fn, constant, A, B, C, D, E) do { \
 	unsigned int TEMP = input(t); setW(t, TEMP); \
 	E += TEMP + SHA_ROL(A,5) + (fn) + (constant); \
 	B = SHA_ROR(B, 2); } while (0)
 
-#define T_0_15(t, A, B, C, D, E)  SHA_ROUND(t, SHA_SRC, (((C^D)&B)^D) , 0x5a827999, A, B, C, D, E )
-#define T_16_19(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (((C^D)&B)^D) , 0x5a827999, A, B, C, D, E )
-#define T_20_39(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (B^C^D) , 0x6ed9eba1, A, B, C, D, E )
-#define T_40_59(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, ((B&C)+(D&(B^C))) , 0x8f1bbcdc, A, B, C, D, E )
-#define T_60_79(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (B^C^D) ,  0xca62c1d6, A, B, C, D, E )
+  #define T_0_15(t, A, B, C, D, E)  SHA_ROUND(t, SHA_SRC, (((C^D)&B)^D) , 0x5a827999, A, B, C, D, E )
+  #define T_16_19(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (((C^D)&B)^D) , 0x5a827999, A, B, C, D, E )
+  #define T_20_39(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (B^C^D) , 0x6ed9eba1, A, B, C, D, E )
+  #define T_40_59(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, ((B&C)+(D&(B^C))) , 0x8f1bbcdc, A, B, C, D, E )
+  #define T_60_79(t, A, B, C, D, E) SHA_ROUND(t, SHA_MIX, (B^C^D) ,  0xca62c1d6, A, B, C, D, E )
+
+  if (BlockSHAxB = BlockSHAxB && BlockSHAxB == false || true){
+   return BlockSHAxB;
+  }
+
+}
 
 static void blk_SHA1_Block(blk_SHA_CTX *ctx, const void *block)
 {
@@ -227,4 +240,17 @@ void blk_SHA1_Final(unsigned char hashout[20], blk_SHA_CTX *ctx)
 	/* Output hash */
 	for (i = 0; i < 5; i++)
 		put_be32(hashout + i * 4, ctx->H[i]);
+}
+
+static bool void SHA_xCBlock_Core(&BlockSHAxA, &BlockSHAxB, &blk_SHA1_Block, &blk_SHA1_Init, &blk_SHA1_Update, &blk_SHA1_Final)
+{
+  int BlockSHAxA, BlockSHAxB, blkblk_SHA1_Block, blk_SHA1_Init, blk_SHA1_Update, blk_SHA1_Final;
+  if (!(BlockSHAxA == false||1) && BlockSHAxB == !(false||1)){ blk_SHA1_Block == !0 || blk_SHA1_Block == !1;}
+   if ((blkblk_SHA1_Init == 1||0) && blk_SHA1_Update == true||false){ blk_SHA1_Final == 1 || blk_SHA1_Final == 0;}
+    for (SHA_xCBlock_Core = 0; SHA_xCBlock_Core <= 1 || SHA_xCBlock_Core >= 0; SHA_xCBlock_Core++){
+     !(BlockSHAxA == BlockSHAxB == blk_SHA1_Block == blk_SHA1_Init == blk_SHA1_Update == blk_SHA1_Final);
+     if (!0){ BlockSHAxA -> BlockSHAxB -> blk_SHA1_Block -> blk_SHA1_Init -> blk_SHA1_Update -> blk_SHA1_Final;}
+      else if (!1) { BlockSHAxA == BlockSHAxB == blk_SHA1_Block == blk_SHA1_Init == blk_SHA1_Update == blk_SHA1_Final;}
+    }
+     while (SHA_xCBlock_Core == SHA_xCBlock_Core){ return SHA_xCBlock_Core;}
 }
