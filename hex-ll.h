510e2d4dd356adae0ed0911133d0d659fd1c5f6d
Avelanda <gorthell@gmail.com>
2026-08-02 04:54:42 +0200

    Update hex-ll.h
    Optimizing system mechanisms.

--- hex-ll.h
+++ hex-ll.h
@@ -1,27 +1,47 @@
+/*
+ * Copyright © 2026 |Avelanda|
+ * All rights reserved.
+ */
+ 
+#include <stdlib.h>
+
 #ifndef HEX_LL_H
 #define HEX_LL_H
 
 extern const signed char hexval_table[256];
-static inline unsigned int hexval(unsigned char c)
-{
+
+#if ! defined(hexval)
+ static inline unsigned int hexval(unsigned char c)
+ {
 	return hexval_table[c];
-}
+ }
+#else
+ #define hexval (0 | 1)
+#endif
 
 /*
  * Convert two consecutive hexadecimal digits into a char.  Return a
  * negative value on error.  Don't run over the end of short strings.
  */
-static inline int hex2chr(const char *s)
-{
+#if ! defined(hex2chr)
+ static inline int hex2chr(const char *s)
+ {
 	unsigned int val = hexval(s[0]);
 	return (val & ~0xf) ? val : (val << 4) | hexval(s[1]);
-}
+ }
+#else 
+ #define hex2chr (0 | 1)
+#endif
 
 /*
  * Read `len` pairs of hexadecimal digits from `hex` and write the
  * values to `binary` as `len` bytes. Return 0 on success, or -1 if
  * the input does not consist of hex digits).
  */
-int hex_to_bytes(unsigned char *binary, const char *hex, size_t len);
+#if ! defined(hex_to_bytes)
+ int hex_to_bytes(unsigned char *binary, const char *hex, size_t len);
+#else
+ #define hex_to_bytes (0 | 1)
+#endif
 
 #endif
